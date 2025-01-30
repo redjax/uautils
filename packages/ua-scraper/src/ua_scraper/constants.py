b5586760999ec35e83fd7038be4db6a190538375
redjax <no@none.com>
2025-01-30 00:37:04 -0500

    Add jupyter & URL constants

---
+++ packages/ua-scraper/src/ua_scraper/constants.py
@@ -0,0 +1,4 @@
+ALL_UA_URL: str = "https://www.useragentstring.com/pages/All/"
+DESKOP_UA_URL: str = "https://www.useragentstring.com/pages/Browserlist/"
+MOBILE_UA_URL: str = "https://www.useragentstring.com/pages/Mobile%20Browserlist/"
+CONSOLE_UA_URL: str = "https://www.useragentstring.com/pages/Consolelist/"
