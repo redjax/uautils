0b5dd07fbdea35bc9335eea841b922d2cb3a725d
redjax <none@none.com>
2025-01-29 23:24:59 -0500

    Modify scrape script, save HTML with UA results

---
+++ packages/domain/src/domain/uastring_domain/schemas.py
@@ -0,0 +1,18 @@
+from loguru import logger as log
+import typing as t
+
+from pydantic import BaseModel, Field, field_validator, ValidationError, computed_field
+
+
+class UAPageScrapeBase(BaseModel):
+    category: str
+    url: str
+    html: t.Union[str, bytes] | None = Field(default=None, repr=False)
+    user_agents: list[str] | None = Field(default_factory=[], repr=False)
+    
+class UAPageScrapeIn(UAPageScrapeBase):
+    pass
+
+
+class UAPageScrapeOut(UAPageScrapeBase):
+    id: int
--- sandbox/scrape/scrape_test2.py
+++ sandbox/scrape/scrape_test2.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import typing as t
 import json
 
+from domain import uastring_domain
+
 import settings
 import setup
 import http_lib
@@ -16,7 +18,7 @@ if not html_cache_dir.exists():
     html_cache_dir.mkdir(parents=True, exist_ok=True)
 
 
-def main(urls: dict[str, dict[str, str]], html_cache_dir: str = html_cache_dir):
+def main(urls: list[dict[str,str]], html_cache_dir: str = html_cache_dir):
     log.debug(f"URLs dict: {urls}")
     
     headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"}
@@ -25,12 +27,12 @@ def main(urls: dict[str, dict[str, str]], html_cache_dir: str = html_cache_dir):
     all_user_agents: list[dict[str, t.Union[str, dict[str, str]]]] = []
     soups: list[bs4.BeautifulSoup] = []
     
-    for ua_dict_name in urls:
-        log.info(f"Scraping {ua_dict_name} UA strings")
+    for ua_dict in urls:
+        log.info(f"Scraping {ua_dict['category']} UA strings")
         
-        ua_type = ua_dict_name
-        ua_page_url = urls[ua_type]["url"]        
-        log.debug(f"UA page type: {ua_type}, URL: {ua_page_url}")
+        ua_category = ua_dict["category"]
+        ua_page_url = ua_dict["url"]        
+        log.debug(f"UA category: {ua_category}, URL: {ua_page_url}")
         
         req = http_lib.build_request(url=ua_page_url, headers=headers)
         
@@ -63,30 +65,41 @@ def main(urls: dict[str, dict[str, str]], html_cache_dir: str = html_cache_dir):
         
         ## Loop over lists of UA strings
         for ua_list in ua_lists:
+            log.debug("Extracting <a> tags from soup")
             ## Grab all links
             links = ua_list.find_all("a")
             # log.debug(f"Found links ({type(links)}): {links}")
             
+            log.debug("Creating list of UA strings from soup")
             link_texts = [l.text for l in links]
             
+            log.debug(f"Create results object, prettify 'soup' objects")
             ## Add links to results
-            result = {"type": ua_type, "user_agents": link_texts}
+            result = {"category": ua_category, "user_agents": link_texts, "html": soup, "url": ua_page_url}
+            log.debug(f"Append user agent scrape results to list")
             all_user_agents.append(result)
             
     log.info(f"Found [{len(all_user_agents)}] UA string(s)")
-    log.debug(f"Example UA strings: {all_user_agents[:5]}")
+    # log.debug(f"Example UA strings: {all_user_agents[:5]}")
     
-    with open("./sandbox/scrape/all_ua_strings.json", "w") as f:
-        data = json.dumps(all_user_agents, sort_keys=True, indent=4, default=str)
-        f.write(data)
+    log.info(f"Saving to ./sandbox/scrape/all_ua_strings.json")
+    try:
+        with open("./sandbox/scrape/all_ua_strings.json", "w") as f:
+            modified_user_agents = [d.pop("html") for d in all_user_agents]
+            data = json.dumps(modified_user_agents, sort_keys=True, indent=4, default=str)
+            f.write(data)
+    except Exception as exc:
+        msg = f"({type(exc)}) Error writing to file './sandbox/scrape/all_ua_strings.json'. Details: {exc}"
+        log.error(msg)
+        raise
 
     
 if __name__ == "__main__":
-    ua_urls: dict = {
-        "browsers": {"name": "browser_uas","url": "https://www.useragentstring.com/pages/Browserlist/"},
-        "mobile": {"name": "mobile_browser_uas", "url": "https://www.useragentstring.com/pages/Mobile%20Browserlist/"},
-        "console": {"name": "console_uas", "url": "https://www.useragentstring.com/pages/Consolelist/"}
-    }
+    ua_urls: list[dict[str, str]] = [
+        {"category": "desktop", "url": "https://www.useragentstring.com/pages/Browserlist/"},
+        {"category": "mobile", "url": "https://www.useragentstring.com/pages/Mobile%20Browserlist/"},
+        {"category": "console", "url": "https://www.useragentstring.com/pages/Consolelist/"}
+    ]
     
     setup.setup_loguru_logging(log_level=settings.LOGGING_SETTINGS.get("LOG_LEVEL", default="INFO"), colorize=True)
     main(urls=ua_urls)
